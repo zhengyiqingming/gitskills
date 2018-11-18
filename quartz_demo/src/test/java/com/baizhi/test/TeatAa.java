51003b53ba85b4b04d67f8e71b530c57c4c68c55
zhengyiqingming <751363075@qq.com>
2018-11-18 19:26:20 +0800

    Test AA BB

---
+++ quartz_demo/src/test/java/com/baizhi/test/TeatAa.java
@@ -0,0 +1,9 @@
+package com.baizhi.test;
+
+import java.sql.SQLOutput;
+
+public class TeatAa {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
---
+++ quartz_demo/src/test/java/com/baizhi/test/TestBB.java
@@ -0,0 +1,7 @@
+package com.baizhi.test;
+
+public class TestBB {
+    public static void main(String[] args) {
+
+    }
+}
